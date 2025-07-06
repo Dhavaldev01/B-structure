02ac9253c3ad7964ba7809eed018d24dee0dd81a
Dhavaldev01 <dhavalpatel97230@gmail.com>
2025-07-06 12:12:53 +0530

    cors add

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+
 
 const UserRoutes = require('./Routes/UserRoutes.js');
 const ProductRoutes = require('./Routes/ProductRoutes.js')
 const morgan = require('morgan');
 
 app.use(express.json());
+app.use(cors());
 // app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const app = require("./app");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
 const db = process.env.MONGO_URL;
